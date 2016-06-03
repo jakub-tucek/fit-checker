d9cc8bfa4b8222d5114c38ce67084ef90f9be12e
Jakub Tucek <jakub.tucek@gmail.com>
2016-06-03 12:58:22 +0200

    Base project created

---
+++ fit-checker-2.0/fit-checker-2.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fit-checker-2.0
+//
+//  Created by jakubtucek on 03/06/16.
+//  Copyright © 2016 jakubtucek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
