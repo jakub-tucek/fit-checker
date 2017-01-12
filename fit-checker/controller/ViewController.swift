71e508ef880a4e644fb78521533e523e0be88233
Josef Dolezal <pepik.ml@gmail.com>
2017-01-12 18:00:59 +0100

    Recreate app project

---
+++ fit-checker/controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fit-checker
+//
+//  Created by Josef Dolezal on 12/01/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
