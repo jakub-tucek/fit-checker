81ec8403f41dcc1ddba407fe983ec4f6fb0746c0
Jakub Tucek <jakub.tucek@gmail.com>
2017-01-14 10:23:42 +0100

    Fix project structure

---
+++ fit-checker/utils/edux-parser/LectureListParser.swift
@@ -0,0 +1,10 @@
+//
+//  LectureListParser.swift
+//  fit-checker
+//
+//  Created by Jakub Tucek on 14/01/17.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
---
+++ fit-checker/utils/edux-parser/LectureListParsing.swift
@@ -0,0 +1,15 @@
+//
+//  LectureListParsing.swift
+//  fit-checker
+//
+//  Created by Jakub Tucek on 14/01/17.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+protocol LectureListParsing {
+
+
+    
+}
