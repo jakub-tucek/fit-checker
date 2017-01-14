557f36fd04c3682a3797cd8acb4079341111c3fc
Jakub Tucek <jakub.tucek@gmail.com>
2017-01-14 13:39:46 +0100

    Extract fileloader to util class

---
+++ fit-checkerTests/utils/edux-parser/LectureListParserTest.swift
@@ -0,0 +1,32 @@
+//
+//  LectureListParserTest.swift
+//  fit-checker
+//
+//  Created by Jakub Tucek on 14/01/17.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import XCTest
+
+class LectureListParserTest: XCTestCase {
+
+    let parser = ClassificationParser()
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+
+    func testHomepage() {
+        
+    }
+    
+}
