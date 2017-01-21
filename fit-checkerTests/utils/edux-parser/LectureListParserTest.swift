c7ecd2f3dd1e2622f677923b46700db57f785b49
Jakub Tucek <jakub.tucek@gmail.com>
2017-01-21 19:25:20 +0100

    Remove empty test functions

--- fit-checkerTests/utils/edux-parser/LectureListParserTest.swift
+++ fit-checkerTests/utils/edux-parser/LectureListParserTest.swift
@@ -12,18 +12,6 @@ class LectureListParserTest: XCTestCase {
 
     let parser = LectureListParser()
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
     func convertToDictionary(text: String) -> [String: Any?]? {
         if let data = text.data(using: .utf8) {
             do {
