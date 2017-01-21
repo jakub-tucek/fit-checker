a463237e50b35fce533e8d2c1e0d21c76c3776e1
Jakub Tucek <jakub.tucek@gmail.com>
2017-01-21 13:19:27 +0100

    Add ChangeDetecting generic protocol

---
+++ fit-checker/utils/change-detect/LectureListDetect.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  fit-checker
+//
+//  Created by Jakub Tucek on 21/01/17.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+class LectureListDetect {
+
+
+    func detectLectureListChange(result : LectureListResult) -> Bool {
+        
+    }
+}
