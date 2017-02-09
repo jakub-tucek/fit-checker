2125dabe4ca443c3c74fd739f81c8332542cea87
Josef Dolezal <pepik.ml@gmail.com>
2017-02-09 01:01:08 +0100

    Add splash screen to login transition

--- fit-checker/controller/LoginViewController.swift
+++ fit-checker/controller/LoginViewController.swift
@@ -35,8 +35,9 @@ class LoginViewController: UIViewController {
 
         /// Login button height
         static let buttonHeight: CGFloat = 33
-
     }
+
+    //MARK: Properties
 
     /// View with username and password fields
     private let loginView = LoginView()
@@ -52,12 +53,14 @@ class LoginViewController: UIViewController {
         return button
     }()
 
+    /// CVUT logo view
     private let logoImageView = UIImageView(image: #imageLiteral(resourceName: "cvut-logo"))
-
-    //MARK: Properties
 
     /// Injected network controller
     private let networkController: NetworkController
+
+    /// Logo top margin constraint
+    private var logoTopConstraint: NSLayoutConstraint?
 
     //MARK: Initialization
 
@@ -79,6 +82,11 @@ class LoginViewController: UIViewController {
         view.backgroundColor = .white
 
         configureView()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showLoginForm()
     }
 
     /// Handles user login action
@@ -103,13 +111,17 @@ class LoginViewController: UIViewController {
     /// Configures all subviews layouts
     private func configureView() {
         let views = [logoImageView, loginView, loginButton]
+        let topMargin = (view.frame.height / 2.0)
+            - (logoImageView.frame.height / 2.0)
 
         view.backgroundColor = UIColor.CVUT.blue
         loginView.backgroundColor = .white
         loginView.layer.masksToBounds = true
         loginView.layer.cornerRadius = 3
+        loginView.alpha = 0
         loginButton.layer.masksToBounds = true
         loginButton.layer.cornerRadius = 3
+        loginButton.alpha = 0
 
         loginView.usernameTextField.placeholder = tr(.username)
         loginView.passwordTextField.placeholder = tr(.password)
@@ -119,9 +131,10 @@ class LoginViewController: UIViewController {
             self.view.addSubview(view)
         }
 
+        logoTopConstraint = logoImageView.topAnchor
+            .constraint(equalTo: view.topAnchor, constant: topMargin)
+
         let constraints = [
-            logoImageView.topAnchor.constraint(equalTo:
-                view.topAnchor, constant: Layout.logoOffset),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             loginView.topAnchor.constraint(equalTo:
                 logoImageView.bottomAnchor, constant: Layout.loginOffset),
@@ -140,6 +153,25 @@ class LoginViewController: UIViewController {
                 Layout.buttonHeight)
         ]
 
+        logoTopConstraint?.isActive = true
         constraints.forEach{ $0.isActive = true }
+    }
+
+    /// Shows login UI after app starts
+    private func showLoginForm() {
+        logoTopConstraint?.constant = Layout.logoOffset
+
+        UIView.animateKeyframes(withDuration: 1, delay: 0.2, options: [.calculationModeLinear], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0,
+                               relativeDuration: 1/2.0, animations: {
+
+                self.view.layoutIfNeeded()
+            })
+
+            UIView.addKeyframe(withRelativeStartTime: 1/2.0, relativeDuration: 1/2.0, animations: { 
+                self.loginView.alpha = 1
+                self.loginButton.alpha = 1
+            })
+        }, completion: nil)
     }
 }
